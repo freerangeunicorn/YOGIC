31fb07d588c2eb52ca87b1c644a76b83e44f5e44
Mai <mai.th.hoang@gmail.com>
2020-09-23 17:58:58 +0200

    New build.

---
+++ build/precache-manifest.173fdbb0bcce53c229b523a9b609bd15.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b7228d791851a4682c5c1ec9398ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a73a365dc092102abd6",
+    "url": "/static/css/2.fbec7bc5.chunk.css"
+  },
+  {
+    "revision": "8fc89e6bb021b1983f02",
+    "url": "/static/css/main.5ede99fb.chunk.css"
+  },
+  {
+    "revision": "6a73a365dc092102abd6",
+    "url": "/static/js/2.ac4a9cba.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.ac4a9cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc89e6bb021b1983f02",
+    "url": "/static/js/main.bfd8309e.chunk.js"
+  },
+  {
+    "revision": "dd3c0297c9de345439aa",
+    "url": "/static/js/runtime-main.9cdd9578.js"
+  },
+  {
+    "revision": "12efd5d075d50eff08a18556e8dcd929",
+    "url": "/static/media/mudra.12efd5d0.png"
+  },
+  {
+    "revision": "e21b1bd08d10ab2576ac15244df878c5",
+    "url": "/static/media/pair.e21b1bd0.png"
+  },
+  {
+    "revision": "a1fd0355db843f93c28b9ce54c4bd4a3",
+    "url": "/static/media/yoga.a1fd0355.png"
+  }
+]);
